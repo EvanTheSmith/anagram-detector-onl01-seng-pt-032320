2fe4971e726bd676240fdf6162fcbdee5e2c1223
Evan Smith <professionalemail@zoho.com>
2020-04-15 22:53:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
       second_word = @matcher.split("").sort.join
       
       if first_word == second_word
-        word
+        return word
        end
     end
     check = check.uniq
